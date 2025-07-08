7885c7693d94535db768c4a1192c46d8dc3bdec8
Ujwal1830 <yangalwarujwal1830@gmail.com>
2025-07-09 02:09:41 +0530

    initial commit - react + redux toolkit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './index.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+        <h1 className="text-4xl font-bold mb-4">Welcome to My App</h1>
+
+        <button className="px-6 py-2 bg-cyan-500 text-white rounded hover:bg-cyan-600 transition duration-300">
+          Click Me
+        </button>
+      </div>
+    </>
+  )
+}
+
+export default App
